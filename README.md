6dd3081a5d29e50ea5c0bd5b1b2ca77d9adfd006
sophie <April_Sophie@163.com>
2018-02-13 14:41:39 +0800

    add mocha test

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
 # json-i18n-tool
+
+[![Build Status](https://travis-ci.org/FrontendSophie/json-i18n-tool.svg?branch=master)](https://travis-ci.org/FrontendSophie/json-i18n-tool)
 [![npm: version](https://img.shields.io/npm/v/json-i18n-tool.svg)](https://www.npmjs.com/package/json-i18n-tool)
 [![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg)](https://github.com/prettier/prettier)
 [![Maintainability](https://api.codeclimate.com/v1/badges/33e078b7a8e19484ac3e/maintainability)](https://codeclimate.com/github/FrontendSophie/json-i18n-tool/maintainability)
 
 > Translate JSON File into Other Languages
 
+## Installation
+``` bash
+npm install json-i18n-tool
+```
+
 ## Usage
 ``` bash
-# install
-npm install json-i18n-tool
-
 # generate translated json file
 node index.js --src=... --lang=... --dst=...
 
-# demo
-node index.js --src=./tests/test.json --lang=en --dst=./tests/en-test.json
+# e.g.
+node index.js --src=./test/test.json --lang=en --dst=./test/en-test.json
 ```
---
+++ test/test.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const expect = require('chai').expect;
+const translator = require('../lib/translate');
+
+describe('json-i18n-tool', function () {
+    it("should have an original test json file", function () {
+        const testFile = fs.existsSync('./test/test.json');
+        const testContent = fs.readFileSync('./test/test.json', 'utf8');
+        expect(testFile).to.be.ok;
+        expect(testContent).to.be.equal('{"class": "三年级二班","students": {"sophie": {"interest": ["编程", "音乐"],"note": "技术改变世界"}}}');
+    });
+
+    it("should generate a translated json file for en", function (done) {
+        this.timeout(5000);
+        const testPromise = new Promise(function (resolve, reject) {
+            translator.translate('./test/test.json', 'en', './test/en-test.json');
+            done();
+        });
+        testPromise.then(function () {
+            const enTestFile = fs.existsSync('./test/en-test.json');
+            const enTestContent = fs.readFileSync('./test/en-test.json', 'utf8');
+            expect(enTestFile).to.be.ok;
+            expect(enTestContent).to.be.equal('{"class": "Class 2, grade 3","students": {"sophie": {"interest": ["programming","music"],"note": "Technology changes the world"}}}');
+        });
+    });
+});
